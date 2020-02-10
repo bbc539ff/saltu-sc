8932569d82d633c735c4d160bb8f8c7c99df0afe
ffbbc539 <lilracaz@outlook.com>
2020-02-10 22:17:55 +0800

    feat(*): Add post to profile timeline and followers home timeline in redis.

--- saltu_post/src/main/java/bbc539ff/saltu/post/service/PostService.java
+++ saltu_post/src/main/java/bbc539ff/saltu/post/service/PostService.java
@@ -6,16 +6,18 @@ import bbc539ff.saltu.post.client.MemberClient;
 import bbc539ff.saltu.post.dao.PostDao;
 import bbc539ff.saltu.post.pojo.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 @Service
 public class PostService {
   @Autowired PostDao postDao;
   @Autowired MemberClient memberClient;
+  @Autowired private RedisTemplate<String, String> redisTemplate;
 
   @Autowired SnowFlake snowFlake;
 
@@ -23,6 +25,23 @@ public class PostService {
     post.setPostId(Long.toString(snowFlake.nextId()));
     post.setPostDate(new Date());
     return postDao.save(post);
+  }
+
+  public void addPostToRedis(Post post) {
+    // Add postId to profile: memberId zSet.
+    redisTemplate
+        .opsForZSet()
+        .add("profile: " + post.getMemberId(), post.getPostId(), new Date().getTime());
+    // Add postId to home: followers id zSet.
+    Set<String> followersSet =
+        redisTemplate
+            .opsForZSet()
+            .rangeByScore("followers: " + post.getMemberId(), 0, 1000, 0, 1000);
+    for (String followerId : followersSet) {
+      redisTemplate
+          .opsForZSet()
+          .add("home: " + post.getMemberId(), post.getPostId(), new Date().getTime());
+    }
   }
 
   public Post updatePost(Post post) {
